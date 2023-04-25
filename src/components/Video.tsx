71fa10007a07ff9dc0ce0fcf1de2083ceba9bae0
ArthCaeta <arthcaetano418@gmail.com>
2023-04-24 21:26:57 -0300

    Inscrição via GraphQL e alterações de CSS

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -31,10 +31,7 @@ interface GetLessonBySlugResponse {
         name: string;
       }
   }
-    
 }
-
-
 
 interface VideoProps {
   lessonSlug: string;
@@ -63,25 +60,25 @@ export function Video(props: VideoProps) {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
+              {data?.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-            Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
+              {data?.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img 
               className="h-16 w-16 rounded-full border-2 border-blue-500 "
-              src="https://github.com/diego3g.png" 
+              src={data?.lesson.teacher.avatarURL}
               alt="" 
               />
 
               <div className="leading-relaxed">
               <strong className="font-bold text-2xl block">
-                Diego Fernandes
+                {data?.lesson.teacher.name}
               </strong>
               <span className="text-gray-200 text-sm block">
-                CTO @Rocketseat
+                {data?.lesson.teacher.bio}
               </span>
               </div>
             </div>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/clgcjbcdl4a5o01t3dllcctox/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization' : `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
